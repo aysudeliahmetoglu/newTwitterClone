3f1a0cd0a3b300ec451905a81dab3e28fb44ce72
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2023-02-21 22:25:31 +0300

    registered and ordered  oinks

--- apps/feed/models.py
+++ apps/feed/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Oink(models.Model):
+    body = models.CharField(max_length=255)
+
+    created_by = models.ForeignKey(User,related_name='oinks',on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ('-created_at',)
