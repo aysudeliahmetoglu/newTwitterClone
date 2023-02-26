66f4506d3047ad9dfd1a9181f8e3226389f03868
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2023-02-26 23:48:24 +0300

    Vue.js posting methods and get posts from database

--- apps/feed/views.py
+++ apps/feed/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Oink
 
 @login_required
 def feed(request):
-    return render(request,'feed/feed.html')
+    userids = [request.user.id]
+
+    for oinker in request.user.oinkerprofile.follows.all():
+        userids.append(oinker.user.id)
+
+    oinks = Oink.objects.filter(created_by_id__in=userids)  
+    return render(request,'feed/feed.html', {'oinks':oinks})
